eba4331862b4629ea0033f433f21cfce2c5d474c
Luke Rogers <lukeroge@gmail.com>
2012-03-05 07:48:22 +1300

    added help string, minor tweaks

--- plugins/steamid.py
+++ plugins/steamid.py
@@ -1,18 +1,16 @@
 from util import hook, steam
-reload(steam)
 from util.steam import SteamCommunityProfile, SteamIDError
-
 
 @hook.command
 def steamid(inp):
-    ".steamid -- stuff"
+    ".steamid <user> -- Gets information on <user> using the Steam API"
     try:
-      profile = SteamCommunityProfile.input_to_profile(inp)
+        profile = SteamCommunityProfile.input_to_profile(inp)
     except SteamIDError,e:
-      return str(e)
+        return str(e)
     
     if profile is None:
-      return "could not recognize your input."
+        return "Could not recognize your input."
     else:
-      return str(profile)
+        return str(profile)
 
